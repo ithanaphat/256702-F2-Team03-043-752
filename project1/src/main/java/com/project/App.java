f69e205b9c2b67d16146cfe43171a6e8285f4266
Thanaphat Janjom <thanaphatchunchom@gmail.com>
2025-02-06 17:55:30 +0700

    .

---
+++ project1/src/main/java/com/project/App.java
@@ -0,0 +1,13 @@
+
+package com.project;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+
+    
+
