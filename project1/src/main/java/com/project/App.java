2adb49e6beac71235bdf8fcb26e666da168d0e5b
Thanaphat Janjom <thanaphatchunchom@gmail.com>
2025-02-10 15:26:38 +0700

    send 2

--- project1/src/main/java/com/project/App.java
+++ project1/src/main/java/com/project/App.java
@@ -1,13 +1,64 @@
-
 package com.project;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.stage.Stage;
+import java.util.Map;
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.dsl.FXGL;
+import com.almasb.fxgl.texture.Texture;
 
+import javafx.scene.text.Text;
 
-    
+public class App extends GameApplication {
+    private Player player;
 
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    protected void initSettings(GameSettings settings) {
+        settings.setWidth(1600);
+        settings.setHeight(800);
+        settings.setTitle("The Last Adventurer");
+        settings.setVersion("0.1");
+    }
+
+    @Override
+    protected void initGame() {
+        FXGL.getGameWorld().addEntity(Background.createBackground());
+        player = new Player(300, 300); // สร้างตัวละคร
+        player.registerControls(); // ลงทะเบียนปุ่มกด
+       
+    }
+
+    @Override
+    protected void initInput() {
+       
+    }
+
+    @Override
+    protected void initGameVars(Map<String, Object> vars) {
+        vars.put("pixelsMoved", 0);
+    }
+
+    @Override
+    protected void initUI() {
+        Text textLabel = new Text("Moved: ");
+        Text textPixels = new Text();
+
+        textLabel.setTranslateX(30);
+        textLabel.setTranslateY(50);
+        textPixels.setTranslateX(80);
+        textPixels.setTranslateY(50);
+
+        textPixels.textProperty().bind(
+                FXGL.getWorldProperties().intProperty("pixelsMoved").asString()
+        );
+
+        FXGL.getGameScene().addUINode(textLabel);
+        FXGL.getGameScene().addUINode(textPixels);
+
+        
+
+    }
+}
---
+++ project1/src/main/java/com/project/Background.java
@@ -0,0 +1,17 @@
+package com.project;
+
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.dsl.FXGL;
+import javafx.scene.image.Image;
+
+public class Background {
+
+    // เมธอดสร้างพื้นหลัง
+    public static Entity createBackground() {
+        return FXGL.entityBuilder()
+                .view(FXGL.texture("1.png"))  // ใช้ texture ของภาพ
+                .at(0, 0)  // ตำแหน่งที่ภาพจะปรากฏในฉาก
+                .build();
+    }
+}
+
