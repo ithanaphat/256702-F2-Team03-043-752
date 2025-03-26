d77923933712683260a564dd23d22bfd33fd8702
Thanaphat Janjom <thanaphatchunchom@gmail.com>
2025-03-26 22:56:04 +0700

    debug

--- project1/src/main/java/com/project/PauseMenu.java
+++ project1/src/main/java/com/project/PauseMenu.java
@@ -113,60 +113,7 @@ public class PauseMenu extends FXGLMenu {
     }
 
     private void loadPlayerFromSave(SaveData data) {
-        if (data == null) {
-            FXGL.getNotificationService().pushNotification("No Save Data Found!");
-            return;
-        }
-    
-        FXGL.getGameWorld().getEntitiesByType(EntityType.PLAYER).forEach(Entity::removeFromWorld);
-        FXGL.getGameWorld().getEntitiesByType(EntityType.MONSTER).forEach(Entity::removeFromWorld);
-        FXGL.getGameWorld().getEntitiesByType(EntityType.BOSS).forEach(Entity::removeFromWorld);
-    
-        Player player = new Player("playerimage.png");
-        Entity newPlayer = player.createPlayer(data.getHealth(), data.getAttack(), data.getLevel(),
-                data.getExperience());
-    
-        newPlayer.setPosition(data.getPosX(), data.getPosY());
-    
-        Stats stats = newPlayer.getComponent(Stats.class);
-        stats.setHealth(data.getHealth());
-        stats.setMaxHealth(data.getMaxHealth());
-        stats.setAttack(data.getAttack());
-        stats.setLevel(data.getLevel());
-        stats.setExperience(data.getExperience());
-    
-        FXGL.set("playerStats", stats);
-        UIManager uiManager = FXGL.geto("uiManager");
-        uiManager.reloadStats();
-        SkillSystem skillSystem = FXGL.geto("skillSystem");
-        skillSystem.setPlayer(player);
-    
-        for (Point2D pos : data.getEnemyPositions()) {
-            spawn("monster", pos.getX(), pos.getY());
-        }
-    
-        if (data.getBossHealth() > 0) {
-            Entity boss = spawn("boss", data.getBossPosX(), data.getBossPosY());
-            Health bossHealth = boss.getComponent(Health.class);
-            if (bossHealth != null) {
-                bossHealth.setValue(data.getBossHealth());
-            }
-            ((App) FXGL.getApp()).setBossSpawned(true); // ตั้งค่าสถานะ bossSpawned
-        } else {
-            ((App) FXGL.getApp()).setBossSpawned(false); // ตั้งค่าสถานะ bossSpawned
-        }
-    
-        FXGL.getNotificationService().pushNotification("Game Loaded!");
-        FXGL.<UIManager>geto("uiManager").updateHealthDisplay();
-        ((App) FXGL.getApp()).reloadStatsAfterLoad();
-    
-        // Rebind the camera to the new player entity
-        FXGL.getGameScene().getViewport().bindToEntity(newPlayer, FXGL.getAppWidth() / 2.0, FXGL.getAppHeight() / 2.0);
-        FXGL.getGameScene().getViewport().setBounds(-200, -200, 1500, 900); // Adjust the bounds as needed
-        FXGL.getGameScene().getViewport().setZoom(1); // ซูมเข้าหน้าจอ
-    
-        // เรียกใช้การ spawn มอนสเตอร์ใหม่
-        ((App) FXGL.getApp()).startMonsterSpawnTask();
+        GameLoader.loadPlayerFromSave(data);
     }
 
     
--- project1/src/main/java/com/project/StartScreen.java
+++ project1/src/main/java/com/project/StartScreen.java
@@ -102,39 +102,7 @@ public class StartScreen extends FXGLMenu {
         FXGL.getGameController().startNewGame();
 
         FXGL.runOnce(() -> {
-            // Remove existing player entities to avoid duplication
-            FXGL.getGameWorld().getEntitiesByType(EntityType.PLAYER).forEach(Entity::removeFromWorld);
-
-            Player player = new Player("playerimage.png");
-            Entity newPlayer = player.createPlayer(data.getHealth(), data.getAttack(), data.getLevel(),
-                    data.getExperience());
-            newPlayer.setPosition(data.getPosX(), data.getPosY());
-
-            Stats stats = newPlayer.getComponent(Stats.class);
-            stats.setHealth(data.getHealth());
-            stats.setMaxHealth(data.getMaxHealth());
-            stats.setAttack(data.getAttack());
-            stats.setLevel(data.getLevel());
-            stats.setExperience(data.getExperience());
-
-            FXGL.set("playerStats", stats);
-            FXGL.<UIManager>geto("uiManager").reloadStats();
-
-            SkillSystem skillSystem = FXGL.geto("skillSystem");
-            skillSystem.setPlayer(player);
-
-            for (Point2D pos : data.getEnemyPositions()) {
-                FXGL.spawn("monster", pos.getX(), pos.getY());
-            }
-
-            // Rebind the camera to the new player entity
-            FXGL.getGameScene().getViewport().bindToEntity(newPlayer, FXGL.getAppWidth() / 2.0, FXGL.getAppHeight() / 2.0);
-            FXGL.getGameScene().getViewport().setBounds(-200, -200, 1500, 900); // Adjust the bounds as needed
-            FXGL.getGameScene().getViewport().setZoom(1); // ซูมเข้าหน้าจอ
-
-
-            FXGL.getNotificationService().pushNotification("Game Loaded!");
-            ((App) FXGL.getApp()).reloadStatsAfterLoad();
+            GameLoader.loadPlayerFromSave(data);
         }, Duration.seconds(0.5));
     }
 }
